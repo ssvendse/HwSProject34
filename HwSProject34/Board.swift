2c401608a73918a7f7af2adc3c4988e2887639eb
Skyler Svendsen <skylersvendsen@gmail.com>
2017-10-11 23:32:42 -0500

    first commit

---
+++ HwSProject34/Board.swift
@@ -0,0 +1,13 @@
+//
+//  Board.swift
+//  HwSProject34
+//
+//  Created by Skyler Svendsen on 10/11/17.
+//  Copyright © 2017 Skyler Svendsen. All rights reserved.
+//
+
+import UIKit
+
+class Board: NSObject {
+
+}
